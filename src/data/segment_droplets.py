5579943f897b6acd199ddd3d18550000afb3bbcc
caromccue <53535917+caromccue@users.noreply.github.com>
2022-03-07 12:40:06 -0500

    deleted scikit segment

--- src/data/segment_droplets.py
+++ src/data/segment_droplets.py
@@ -37,70 +37,7 @@ from scipy import ndimage as ndi
 
 from src.data.utils import select_rectangle, open_grey_scale_image, crop, clear_border
 
-
-def segment_skimage(img, exp_clip_limit=0.06, closing_disk_radius=4, rm_holes_area=8192, minima_minDist=100, mask_val=0.1, postsize = 85):
-    '''
-    Segments droplets in an image using a watershed algorithm. Scikit-image implementation.
-
-    Parameters
-    ----------
-    img: numpy.ndarray
-        Array representing the greyscale values (0-255) of an image cropped to show only the droplets region
-    exp_clip_limit: float [0-1], optional
-        clip_limit parameter for adaptive equalization
-    closing_disk_radius: int, optional
-        diamater of selection disk for the closing function
-    rm_holes_area: int, optional
-        maximum area of holes to remove
-    minima_minDist: int, optional
-        minimum distance between peaks in local minima determination
-    mask_val: float, optional
-        Masking value (0-1) for the distance plot to remove small regions. Default 0.2
-    postsize: size of posts in pixels, default 85 (for 5 um posts)
-    Returns
-    -------
-    (labeled: numpy.ndarray, num_regions: int)
-        labeled: labeled array of the same shape as input image where each region is assigned a disctinct integer label.
-        num_regions: number of labeled regions
-    '''
-
-    # Adaptive equalization
-    img_adapteq = equalize_adapthist(img, clip_limit=exp_clip_limit)
-
-    # Minimum threshold
-    threshold = threshold_otsu(img_adapteq)
-
-    binary = img_adapteq > threshold
-
-    # Remove dark spots and connect bright spots
-    closed = binary_closing(binary, selem=disk(closing_disk_radius))
-    rm_holes_closed = remove_small_holes(closed, area_threshold=rm_holes_area, connectivity=2)
-
-    # Calculate the distance to the dark background
-    distance = ndi.distance_transform_edt(rm_holes_closed)
-
-    # Increase contrast of the the distance image
-    cont_stretch = exposure.rescale_intensity(distance, in_range='image')
-
-    # Mask the distance image to remove interstitial points
-    masked = cont_stretch.copy()
-    masked[masked < mask_val] = 0
-
-    # Find local maximas of the distance image
-    local_maxi = peak_local_max(masked, indices=False, min_distance=minima_minDist)
-
-    # Markers for watershed are the local maxima of the distance image
-    markers, num_maxima = ndi.label(local_maxi)
-
-    # Run watershed algorithm on the inverse of the distance image
-    segmented = watershed(-masked, markers, mask = masked > 0)
-
-    # Label the segments of the image
-    labeled, num_regions = ndi.label(segmented)
-
-    return (labeled, num_regions)
-
-def segment(img, exp_clip_limit=15):
+def segment(img, exp_clip_limit=15, postsize=85):
     '''
     Segments droplets in an image using a watershed algorithm. OpenCV implementation.
 
@@ -145,20 +82,20 @@ def segment(img, exp_clip_limit=15):
     # Finding sure foreground area
     #dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
     dist_transform = cv2.distanceTransform(closing,cv2.DIST_L2,5)
-    ret, sure_fg = cv2.threshold(dist_transform,0.15*dist_transform.max(),255,0)
+    _, sure_fg = cv2.threshold(dist_transform,0.15*dist_transform.max(),255,0)
     # Finding unknown region
     sure_fg = np.uint8(sure_fg)
     unknown = cv2.subtract(sure_bg,sure_fg)
 
     # Marker labelling
-    ret, markers = cv2.connectedComponents(sure_fg)
+    _, markers = cv2.connectedComponents(sure_fg)
     # Add one to all labels so that sure background is not 0, but 1
     markers = markers+1
     # Now, mark the region of unknown with zero
     markers[unknown > 0] = 0
 
     # Run the watershed algorithm
-    three_channels = cv2.cvtColor(fill_holes, cv2.COLOR_GRAY2BGR)
+    three_channels = cv2.cvtColor(closed, cv2.COLOR_GRAY2BGR)
     segmented = cv2.watershed(three_channels.astype('uint8'), markers)
 
     return (segmented, segmented.max()-1)
@@ -217,7 +154,7 @@ def extract_indiv_droplets(img, labeled, border=15, area_upper_cutoff=0.9, area_
 def segment_droplets_to_file(image_filename, crop_box=None, save_overlay=False):
 
     if os.path.isdir(image_filename):
-        img_list = [os.path.join(image_filename,f) for f in os.listdir(image_filename) if f.endswith('.JPG')]
+        img_list = [os.path.join(image_filename,f) for f in os.listdir(image_filename) if f.endswith('.tiff')]
     elif os.path.isfile(image_filename):
         img_list = [image_filename]
 
@@ -261,7 +198,7 @@ def segment_droplets_to_file(image_filename, crop_box=None, save_overlay=False):
 
         # Save all the images in the output directory
         for (i, img) in enumerate(drop_images):
-            name = os.path.join(out_directory, os.path.basename(image_file).split('.')[0] + '_drop_' + str(i) + '.jpg')
+            name = os.path.join(out_directory, os.path.basename(image_file).split('.')[0] + '_cell_' + str(i) + '.jpg')
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 io.imsave(name, img, check_contrast=False)
